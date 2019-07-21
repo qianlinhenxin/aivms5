6b6f8dc2cd536b2ab38b8c21913d8c742c0c2f83
xigexb <xigexb@outlook.com>
2019-07-21 12:44:19 +0800

    依赖

--- aivms5-service/logger-service/logger-realize/src/main/java/com/qlhx/service/logger/realize/mq/MQConfig.java
+++ aivms5-service/logger-service/logger-realize/src/main/java/com/qlhx/service/logger/realize/mq/MQConfig.java
@@ -2,9 +2,9 @@ package com.qlhx.service.logger.realize.mq;
 
 
 import com.qlhx.base.model.MQLogger;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sun.misc.Queue;
 
 @Configuration
 public class MQConfig {
