c3bb7b99070a4df57eaa08aa4333e7f9f3db0e0f
xigexb <xigexb@outlook.com>
2019-07-21 12:07:13 +0800

    增加

--- aivms5-service/control-service/control-realize/src/main/java/com/qlhx/service/control/realize/main/ServiceControlApplication.java
+++ aivms5-service/control-service/control-realize/src/main/java/com/qlhx/service/control/realize/main/ServiceControlApplication.java
@@ -1,5 +1,6 @@
 package com.qlhx.service.control.realize.main;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
