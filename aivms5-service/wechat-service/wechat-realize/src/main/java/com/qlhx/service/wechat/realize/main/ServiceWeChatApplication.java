8667ba8c7763893feecbe3cc3834f6ce12a80736
xigexb <xigexb@outlook.com>
2019-07-21 14:10:35 +0800

    增加模块，删除依赖

--- aivms5-service/wechat-service/wechat-realize/src/main/java/com/qlhx/service/wechat/realize/main/ServiceWeChatApplication.java
+++ aivms5-service/wechat-service/wechat-realize/src/main/java/com/qlhx/service/wechat/realize/main/ServiceWeChatApplication.java
@@ -3,9 +3,13 @@ package com.qlhx.service.wechat.realize.main;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 
 @EnableEurekaClient
@@ -16,6 +20,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ServiceWeChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceWeChatApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
 	}
 
 }
---
+++ aivms5-web/base-web/src/main/java/com/qlhx/web/base/main/BaseWebApplication.java
@@ -0,0 +1,40 @@
+package com.qlhx.web.base.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Create by xb
+ * The file is [ WebApplication] on [ x-file-system ] project
+ * The file path is com.itgo.web.main.WebApplication
+ *
+ * @versio 1.0.0
+ * @Author he ming xi
+ * @date 2019/4/7 20:29
+ * @description
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@EnableFeignClients(basePackages = {"com.qlhx.service.auth.api.api"})
+@ComponentScan(basePackages = {"com.qlhx.base"})
+@ServletComponentScan
+@EnableAspectJAutoProxy(exposeProxy=true)
+public class BaseWebApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(BaseWebApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseWebApplication.class, args);
+        logger.info("<-------------auth web start success--------------->");
+    }
+
+
+
+}
