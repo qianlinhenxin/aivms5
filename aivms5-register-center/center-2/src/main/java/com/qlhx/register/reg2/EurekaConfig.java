11fda1ef0a75f733601cf39aed2a0f27e09ccb3c
xigexb <xigexb@outlook.com>
2019-08-03 12:27:47 +0800

    增加安全认证

---
+++ aivms5-register-center/center-2/src/main/java/com/qlhx/register/reg2/EurekaConfig.java
@@ -0,0 +1,22 @@
+package com.qlhx.register.reg2;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Create by xigexb
+ *
+ * @version 1.0.0
+ * @Author xigexb
+ * @date 2019/8/3 12:16
+ * @Description desc:
+ */
+@EnableWebSecurity
+public class EurekaConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
